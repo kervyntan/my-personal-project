a1dc86cb3ad1b2ad192a651f890a2858bfa9d98d
Kervyn <kervyntan@gmail.com>
2022-04-07 23:09:17 +0800

    Added template files for the site

--- mysite/kervyn/views.py
+++ mysite/kervyn/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view (request):
+    return render(request, 'kervyn/index.html')
+
+def about_view (request):
+    return
+
+def journey_view (request):
+    return
